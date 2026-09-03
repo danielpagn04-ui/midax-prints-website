41f1ee274037e51087523406500516fb30834339
Amedu Ugbede Daniel <danielpagn04@gmail.com>
2026-09-03 10:25:19 +0100

    Update Midax website homepage and product catalogue

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ def home():
     return render_template("index.html")
 
 
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/products/sublimation-tote-bag")
+def sublimation_tote_bag():
+    return render_template("product.html")
+
+
+
+@app.route("/products/backdrop-banner")
+def backdrop_banner():
+    return render_template("backdrop.html")
+
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
