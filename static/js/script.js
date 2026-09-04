75f500e9373c170d2d5be80a81f6799463e4bc8e
Amedu Ugbede Daniel <danielpagn04@gmail.com>
2026-09-04 22:23:08 +0100

    Upgrade homepage with premium sections and rotating products

--- static/js/script.js
+++ static/js/script.js
@@ -50,3 +50,67 @@ galleryCategories.forEach(category => {
     }
 
 });
+
+
+/* =========================
+   HERO PRODUCT ROTATION
+========================= */
+
+const heroProductImage = document.querySelector('#heroProductImage');
+const heroProductName = document.querySelector('#heroProductName');
+const heroProductLink = document.querySelector('#heroProductLink');
+
+if (heroProductImage && heroProductName && heroProductLink) {
+
+    const heroProducts = [
+        {
+            name: 'SUBLIMATION TOTE BAG',
+            image: "/static/images/products/sublimation-tote-bag/tote-bag.jpg",
+            alt: 'Midax Prints House Services sublimation tote bag',
+            link: '/products/sublimation-tote-bag'
+        },
+        {
+            name: 'BRANDED T-SHIRTS',
+            image: "/static/images/products/branded-tshirt/branded-tshirt.webp",
+            alt: 'Midax Prints House Services branded T-shirts',
+            link: '/products/branded-tshirt'
+        },
+        {
+            name: 'ROLL-UP BANNER',
+            image: "/static/images/products/rollup-banner/rollup.webp",
+            alt: 'Midax Prints House Services roll-up banner',
+            link: '/products/rollup-banner'
+        },
+        {
+            name: 'WALL CALENDAR',
+            image: "/static/images/products/wall-calendar/wall-calendar.jpg",
+            alt: 'Midax Prints House Services wall calendar',
+            link: '/products/wall-calendar'
+        }
+    ];
+
+    let heroProductIndex = 0;
+
+    setInterval(() => {
+
+        heroProductImage.style.opacity = '0';
+
+        setTimeout(() => {
+
+            heroProductIndex =
+                (heroProductIndex + 1) % heroProducts.length;
+
+            const product = heroProducts[heroProductIndex];
+
+            heroProductImage.src = product.image;
+            heroProductImage.alt = product.alt;
+            heroProductName.textContent = product.name;
+            heroProductLink.href = product.link;
+
+            heroProductImage.style.opacity = '1';
+
+        }, 350);
+
+    }, 5000);
+
+}
